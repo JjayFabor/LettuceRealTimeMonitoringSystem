c0e9d23fe1ce039d2c2baf22c1497458928e897d
JjayFabor <faborjaylordvhan@gmail.com>
2023-10-18 21:41:05 +0800

    Added a calendar chart

--- static/growthPred.js
+++ static/growthPred.js
@@ -31,6 +31,8 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+let predictedGrowthDays; 
+
 // Function to call or get the prediction of the lettuce growth day and display
 function fetchPredictions() {
     // Fetch predictions by making a POST request to the server
@@ -47,7 +49,8 @@ function fetchPredictions() {
         
         // Update the prediction display
         const maxOptimalDays = 45;
-        const predictedGrowthDays = data.predictions; // Use the received prediction value
+        predictedGrowthDays = data.predictions; // Use the received prediction value
+
         document.getElementById('predictionDisplay').innerText = "The predicted value is: " + predictedGrowthDays;
         
         // Calculate the progress percentage based on the received prediction value
@@ -57,9 +60,48 @@ function fetchPredictions() {
         const progressBar = document.getElementById('progress-bar');
         progressBar.style.width = `${progressPercentage}%`;
         progressBar.innerText = `${progressPercentage.toFixed(2)}%`;
+
     })
     .catch(error => {
         console.error('Error fetching predictions:', error);
     });
 }
 
+
+document.addEventListener('DOMContentLoaded', function (){
+    let today = new Date();
+    let predictedHarvestDate = new Date(today);
+    predictedHarvestDate.setDate(today.getDate() + predictedGrowthDays);
+
+    const calendarViewData = {
+        data: [
+            {
+                type: 'scatter',
+                x: [today, predictedHarvestDate],
+                y: [0, 1],
+                mode: 'lines+markers+text',
+                text: ['Today', 'Predicted Harvest Day: ' + predictedHarvestDate.toDateString()],
+                textposition: 'top right',
+                marker: { size: 10 },
+                line: { width: 2 },
+            },
+        ],
+        layout: {
+            xaxis: {
+                title: 'Date',
+                type: 'date',
+                tickformat: '%Y-%m-%d',
+            },
+            yaxis: {
+                title: 'Progress',
+                range: [0, 1],
+            },
+            hovermode: 'closest',
+            title: 'Lettuce Growth Calendar',
+        },
+    };
+
+    // Disable mode bar (zoom and pan buttons)
+    const config = { displayModeBar: false};
+    Plotly.newPlot('calendar-view', calendarViewData.data, calendarViewData.layout, config);
+})
